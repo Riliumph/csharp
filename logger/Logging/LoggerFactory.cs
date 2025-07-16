0361ea31192a01562fdf0612d5ab94ab2fef7311
Riliumph <riliumph@outlook.jp>
2025-07-16 11:26:13 +0900

    [add] formatterを導入
    デフォルトフォーマッタだと強制力が弱かったためCShapierを導入

--- logger/Logging/LoggerFactory.cs
+++ logger/Logging/LoggerFactory.cs
@@ -1,7 +1,7 @@
-using MsLogging = Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Extensions.Logging;
+using MsLogging = Microsoft.Extensions.Logging;
 
 namespace logger.Logging
 {
@@ -9,18 +9,28 @@ namespace logger.Logging
     {
         private static readonly object _lock = new();
         private static MsLogging.ILoggerFactory? _loggerFactory;
+
+        /// <summary>
+        /// インスタンス取得関数
+        /// </summary>
+        /// <typeparam name="T">実行するクラス</typeparam>
+        /// <returns>ロガーインスタンス</returns>
         public static MsLogging.ILogger<T> Get<T>()
         {
             if (_loggerFactory == null)
             {
                 lock (_lock)
-                {   // double-checked locking pattern
+                { // double-checked locking pattern
                     _loggerFactory ??= CreateFactory();
                 }
             }
             return _loggerFactory!.CreateLogger<T>();
         }
 
+        /// <summary>
+        /// ロガーファクトリーの生成関数
+        /// </summary>
+        /// <returns>ロガーファクトリーインスタンス</returns>
         private static MsLogging.ILoggerFactory CreateFactory()
         {
             Log.Logger = new LoggerConfiguration()
--- logger_demo/Program.cs
+++ logger_demo/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using logger_demo.Hoge;
 using logger.Logging;
 using Microsoft.Extensions.Logging;
-using logger_demo.Hoge;
 
 class Program
 {
@@ -13,11 +13,11 @@ class Program
         logger.LogWarning("This is a warning.");
         logger.LogError("This is an error!");
         var ctrl = new HogeController(Logger.Get<HogeController>());
-        var data = new Dictionary<string, object>{
+        var data = new Dictionary<string, object>
+        {
             { "id", 123 },
-            { "name", "TestName" }
+            { "name", "TestName" },
         };
         ctrl.Handle(data);
     }
 }
-
