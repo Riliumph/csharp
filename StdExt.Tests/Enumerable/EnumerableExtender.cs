f60f163dcfd484b42c90222c71fc1bc33176ebef
Riliumph <riliumph@gmail.com>
2025-07-31 18:31:30 +0900

    [add] 標準拡張ライブラリのテストプロジェクト追加

---
+++ StdExt.Tests/Enumerable/EnumerableExtender.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using StdExt.Enumerable;
+using Xunit;
+
+namespace StdExt.Tests.Enumerable
+{
+    public class EnumerableExtenderTests
+    {
+        [Theory]
+        [InlineData(null, true)]
+        [MemberData(nameof(GetTestData))]
+        public void IsNullOrEmpty_VariousInputs_ReturnsExpected(
+            IEnumerable<int> input,
+            bool expected
+        )
+        {
+            var result = input.IsNullOrEmpty();
+            Assert.Equal(expected, result);
+        }
+
+        public static IEnumerable<object[]> GetTestData()
+        {
+            yield return new object[] { new List<int>(), true };
+            yield return new object[]
+            {
+                new List<int> { 1 },
+                false,
+            };
+            yield return new object[] { new int[] { }, true };
+            yield return new object[] { new int[] { 42 }, false };
+            yield return new object[] { GetLazyEnumerable(), false };
+        }
+
+        private static IEnumerable<int> GetLazyEnumerable()
+        {
+            yield return 99;
+        }
+    }
+}
---
+++ StdExt.Tests/README.md
@@ -0,0 +1,23 @@
+# 標準拡張ライブラリ　テストプロジェクト
+
+## プロジェクトの作り方
+
+```console
+$ dotnet new xunit -n StdExt.Tests
+$ cd StdExt.Tests
+$ dotnet add reference ../StdExt/StdExt.csproj
+```
+
+## 使い方
+
+### ビルド方法
+
+```console
+$ dotnet build
+```
+
+### 実行方法
+
+```console
+$ dotnet test
+```
