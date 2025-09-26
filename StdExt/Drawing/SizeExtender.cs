518384817fa799de2ddb9f5a927428bcf41e9076
Riliumph <riliumph@gmail.com>
2025-09-26 15:11:40 +0900

    [update] SizeExtenderをGenericsに対応

--- StdExt/Drawing/SizeExtender.cs
+++ StdExt/Drawing/SizeExtender.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+
 namespace StdExt.Drawing
 {
     public static class SizeExtender
@@ -7,12 +9,15 @@ namespace StdExt.Drawing
         /// </summary>
         /// <param name="size">対象の値</param>
         /// <returns>アスペクト比</returns>
-        public static double AspectRatio(this Size size)
+        public static T AspectRatio<T>(this Size size)
+            where T : INumber<T>
         {
             // 構造体なのでnullチェックは不要
             if (size.Height == 0)
-                return 0.0;
-            return (double)size.Width / size.Height;
+                return T.Zero;
+            var w = T.CreateChecked(size.Width);
+            var h = T.CreateChecked(size.Height);
+            return w / h;
         }
 
         /// <summary>
@@ -22,12 +27,17 @@ namespace StdExt.Drawing
         /// <param name="src"></param>
         /// <param name="dst"></param>
         /// <returns></returns>
-        public static (double, double) ScaleRatio(this Size src, Size dst)
+        public static (T, T) ScaleRatio<T>(this Size src, Size dst)
+            where T : INumber<T>
         {
             if (dst.Width == 0 || dst.Height == 0)
-                return (0.0, 0.0);
-            var scaleX = (double)src.Width / dst.Width;
-            var scaleY = (double)src.Height / dst.Height;
+                return (T.Zero, T.Zero);
+            var srcW = T.CreateChecked(src.Width);
+            var dstW = T.CreateChecked(dst.Width);
+            var scaleX = srcW / dstW;
+            var srcH = T.CreateChecked(src.Height);
+            var dstH = T.CreateChecked(dst.Height);
+            var scaleY = srcH / dstH;
             return (scaleX, scaleY);
         }
     }
