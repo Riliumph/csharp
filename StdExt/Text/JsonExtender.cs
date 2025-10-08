f1121cef18f0ec6817b1753fbcb2ab7a677b7aa9
Riliumph <riliumph@outlook.jp>
2025-10-09 01:24:02 +0900

    [fix] 警告の抑制

--- StdExt/Text/JsonExtender.cs
+++ StdExt/Text/JsonExtender.cs
@@ -1,15 +1,16 @@
+using System.Runtime.CompilerServices;
 using System.Text.Json;
 
 namespace StdExt.Text
 {
     public static class JsonExtender
     {
+        private static readonly Lazy<JsonSerializerOptions> _indentOn = new(() => new JsonSerializerOptions { WriteIndented = true });
+        private static readonly Lazy<JsonSerializerOptions> _indentOff = new(() => new JsonSerializerOptions { WriteIndented = false });
+
         public static string ToJsonString<T>(this T i, bool writeIndent = false)
         {
-            var options = new JsonSerializerOptions
-            {
-                WriteIndented = writeIndent,
-            };
+            var options = writeIndent ? _indentOn.Value : _indentOff.Value;
             return JsonSerializer.Serialize(i, options);
         }
     }
